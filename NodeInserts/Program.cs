bc46bb40cbcf98a52460d37c54df37aa023fbcac
User <User@LAB3-3>
2024-01-18 14:31:51 +0200

    srtb56h

--- NodeInserts/Program.cs
+++ NodeInserts/Program.cs
@@ -3,8 +3,33 @@ using NodeInserts.Models;
 
 namespace NodeInserts
 {
+    
     internal class Program
     {
+        public static bool IsReciever(Node<ShabatRecievers> node, int day, int month, int year)
+        {
+            while (node != null)
+            {
+                if (node.GetValue().GetDay() == day && node.GetValue().GetMonth() == month && node.GetValue().GetYear() == year)
+                    return true;
+                node = node.GetNext();
+            }
+            return false;
+        }
+        public static Node<ShabatRecievers> DeleteReciever(Node<ShabatRecievers> node, string name)
+        {
+            Node<ShabatRecievers> head = node;
+            while(node!=null)
+            {
+                if (node.GetValue().GetParent1() == name)
+                {
+                    head = NodeHelper.DeleteWithDummy(node, node.GetValue());
+                    return head;
+                }
+                node = node.GetNext();
+            }
+            return head;
+        }
         static void Main(string[] args)
         {
             Node<ShabatRecievers> shabatShalom = new Node<ShabatRecievers>(new ShabatRecievers("Shiri","Shira",18,1,2024));
