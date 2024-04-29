8be8aaa29bcff5fbfef0133261d5e07ce052d40c
ShaharShalgi <shshalgi@gmail.com>
2024-04-30 00:03:50 +0300

    wdew

---
+++ NodeInserts/Device.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeInserts
+{
+    public class Device
+    {
+        protected string pwd;
+        protected bool working;
+        public Device(string pwd) 
+        {
+            this.pwd = pwd;
+            this.working = false;
+        }
+    }
+}
--- NodeInserts/Program.cs
+++ NodeInserts/Program.cs
@@ -99,16 +99,75 @@ namespace NodeInserts
             }
             return head;
         }
+        public static int[] GetSides(int x)
+        {
+            int[] sides = new int[2];
+            sides[1] = x % 10;
+            while (x / 10 != 0)
+            {
+                x /= 10;
+            }
+            sides[0] = x;
+            return sides;
+        }
+        public static bool IsSameSides(int[] x1, int[] x2)
+        {
+            if (x1[0] != x2[0] || x1[1] != x2[1]) return false;
+            return true;
+        }
+        
+        public static Node<int> ModifyByX(Node<int> node, int x)
+        {
+            Node<int> head = new Node<int>(default, node); 
+            Node<int> tail = new Node<int>(default, node);
+            bool SameSides = false;
+            int[] val = GetSides(x);
+            while (tail.GetNext()!=null)
+            {
+                if(IsSameSides(val, GetSides(tail.GetNext().GetValue())))
+                {
+                    tail.SetNext(tail.GetNext().GetNext());
+                    SameSides= true;
+                }
+                else
+                {
+                    tail = tail.GetNext();
+                }
+                
+               }
+            if (!SameSides)
+            {
+                int count = NodeHelper.CountList(node);
+                for (int i = 0; i < (count / 2)-1; i++)
+                {
+                    node = node.GetNext();
+                }
+                
+                node.SetNext(new Node<int>(x, node.GetNext()));
+            }
+            return head.GetNext();
+            
+        }
         static void Main(string[] args)
         {
-            Node<ShabatRecievers> shabatShalom = new Node<ShabatRecievers>(new ShabatRecievers("Shiri","Shira",18,1,2024));
-            Node<ShabatRecievers> nextShabat = new Node<ShabatRecievers>(new ShabatRecievers("Ofek", "Aviv", 25, 1, 2024));
-            shabatShalom.SetNext(nextShabat);
+            //Node<ShabatRecievers> shabatShalom = new Node<ShabatRecievers>(new ShabatRecievers("Shiri","Shira",18,1,2024));
+            //Node<ShabatRecievers> nextShabat = new Node<ShabatRecievers>(new ShabatRecievers("Ofek", "Aviv", 25, 1, 2024));
+            //shabatShalom.SetNext(nextShabat);
 
-            ShabatRecievers sh = shabatShalom.GetValue();
-            string p1 = sh.GetParent1();
+            //ShabatRecievers sh = shabatShalom.GetValue();
+            //string p1 = sh.GetParent1();
+            //int[] arr = GetSides(5072);
+            //Console.WriteLine(arr[0]);
+            //Console.WriteLine(arr[1]);
+            Node<int> node4 = new Node<int>(5493);
+            Node<int> node3 = new Node<int>(51, node4);
+            Node<int> node2 = new Node<int>(425, node3);
+            Node<int> node1 = new Node<int>(2705, node2);
+            Console.WriteLine(ModifyByX(node1, 5072).ToString());
 
 
         }
+
+
     }
 }
